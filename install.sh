21d01149a13a9532aaee7396faf69f1cccb77ddc
X E T R A <xetrazx@gmail.com>
2025-05-22 20:44:56 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo pacman -S --needed --noconfirm git
-git clone https://github.com/xetrazxz/dotfiles..git ~/dotfiles
+git clone https://github.com/xetrazxz/dotfiles.git ~/dotfiles
 sh ~/dotfiles/files/setup.sh
